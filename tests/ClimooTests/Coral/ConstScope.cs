8f32cbb0a4523f9185051b573157f0e87aebb06b
Kayateia <github@kayateia.net>
2014-09-01 11:59:10 -0700

    Make ConstScope capable of having parents, and add a method to push a scope on the Coral stack

---
+++ tests/ClimooTests/Coral/ConstScope.cs
@@ -0,0 +1,56 @@
+﻿#region License
+/*
+	CliMOO - Multi-User Dungeon, Object Oriented for the web
+	Copyright (C) 2010-2014 Kayateia
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+namespace Kayateia.Climoo.Tests
+{
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Kayateia.Climoo.Scripting.Coral;
+
+/// <summary>
+/// Tests for the Coral scripting language.
+/// </summary>
+[TestFixture]
+public partial class CoralTest
+{
+	// Works
+	[Test]
+	public void ConstScope()
+	{
+		string program = @"
+a = testconst
+b = """"
+try:
+	testconst = 5
+except exc:
+	b = exc
+";
+		Runner r = new Runner();
+		ConstScope scope = new ConstScope( r.state.scope );
+		scope.setConstant( "testconst", 10 );
+		r.pushScope( scope );
+		runAndDump( "ConstScope", r, program, null );
+	}
+}
+
+}
